45083836ff18a047d70ebc0ceab37e071e15f12d
Ainsley <ainsleymitchum@gmail.com>
2015-09-26 18:16:44 -0700

    Revised mergeFiles function Scrapped the mergeFiles version from earlier script. Also added in comments listing the assignment instructions

--- R_Code/run_analysis.R
+++ R_Code/run_analysis.R
@@ -1,7 +1,117 @@
 # FINAL SCRIPT - run_analysis.R
-# 1. Merges the training and the test sets to create one data set.
-# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
-# 3. Uses descriptive activity names to name the activities in the data set
-# 4. Appropriately labels the data set with descriptive variable names. 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of 
-#    each variable for each activity and each subject.
+library(dplyr)
+
+# Set working directory:
+# <<<Redact before submit?>>>
+setwd("/home/ainsley/Documents/Data_Science_Coursera/Getting_and_Cleaning_Data/GCD_course_project")
+
+######################################################################
+## 1. Merges the training and the test sets to create one data set. ##
+######################################################################
+
+### main data directory
+dataDir <- "./UCI HAR Dataset"
+
+########################################
+# Merge the test and training data #####
+
+#    Merge Function:
+#    Arguments: Data directory, data type (string)
+#    Decided to merge each data type one at a time
+mergeFiles <- function(data_dir,data_type) {
+  
+  # 1. Modify data dir and make sure data_type is all lowercase
+  data_type <- tolower(data_type) # just to be sure
+  data_dir <- paste(data_dir,"/",data_type,"/",sep="")
+  
+  # 2. Load activity labels:
+  # Weak... I just hard coded this
+  act.labels <- read.table("./UCI HAR Dataset/activity_labels.txt",
+                           col.names=c("actNumber","actLabel"))
+  
+  # 3. Load feature labels
+  # Same here
+  features <- read.table("./UCI HAR Dataset/features.txt",
+                         col.names=c("featureNumber","featureLabel"))
+  
+  # 4. Load the data files
+  # 4.1 - subject file
+  # 4.1.1 - find the subject file:
+  subject_file <- list.files(data_dir)[grep("subject", list.files(data_dir))]
+  
+  subs <- read.table(paste(data_dir,subject_file,sep=""),
+                     col.names="subject")
+  
+  # 4.2 data (561 columns)
+  # 4.2.1 - find the X_ data file:
+  X_file <- list.files(data_dir)[grep("X_", list.files(data_dir))]
+  x.data <- read.table(paste(data_dir,X_file,sep=""))
+  # rename x.data columns
+  # Did after file was loaded because col.names were truncated when I col.names
+  # was specified in read.table
+  names(x.data) <- features$featureLabel
+  # 4.2.2 - Find the mean and standard deviation columns
+  meanCols <- grep("mean",names(x.data),ignore.case=T)
+  stdCols <- grep("std",names(x.data),ignore.case=T)
+  # 4.2.3 - Combine and sort the column indexes
+  keepCols <- sort(c(meanCols,stdCols))
+  # 4.2.4 - Drop the unnecessary columns
+  #### Keeps only columns with "mean" or "std" in the column name
+  x.data <- x.data[,keepCols]
+  
+  # 4.3 - Activity labels
+  # 4.3.1 - Find activity labels file
+  y_file <- list.files(data_dir)[grep("y_", list.files(data_dir))]
+  y.data <- read.table(paste(data_dir,y_file,sep=""),
+                     col.names="activity")
+  
+  # 5. Merge activity training labels from dataset with text labels
+  y.data <- merge(y.data,act.labels,by.x="activity",by.y="actNumber")
+  
+  # 6. Merge data with subject and activity labels
+  y.data <- cbind(subs,y.data,x.data)
+  
+  # 7. Add a column to the end marking the type of data this is:
+  y.data$data_type <- data_type
+  ## 7.1 - arrange columns
+  y.data <- y.data[,c(1,2,3,90,4:89)]
+  
+  # 8. Return the merged data set
+  return(y.data)
+}
+
+########################################################################
+# Create two merged files: One for training data and one for test data #
+########################################################################
+mergeTrain <- mergeFiles(dataDir,"train")
+mergeTest <- mergeFiles(dataDir,"test")
+
+################# FINAL MERGE #######################
+###### Merge the training and test data files ######
+dataMerge <- rbind(mergeTrain,mergeTest)
+
+##############################################################################################
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement.##
+##############################################################################################
+## I really don't completely get what this means. I'm assuming it means that I need only 
+## keep those columns in the data frame that have to do with means or standard deviations
+## for the measurements. This is accomplished in the mergeFiles function: Only those columns
+## in X_xxxx.txt that have "mean" or "std" in the column name are retained
+
+##############################################################################
+# 3. Uses descriptive activity names to name the activities in the data set ##
+##############################################################################
+## Accomplishes this by merging the text activity labels from activity_labels.txt
+## With the file y_XXXX.txt for each activity type, which contains numeric activity labels
+
+##########################################################################
+# 4. Appropriately labels the data set with descriptive variable names. ##
+##########################################################################
+## I assume this criteria is met by using the text labels in features.txt
+## to label the columns in the X_xxxx.txt data files for each activity type
+
+#########################################################################################
+# 5. From the data set in step 4, creates a second, independent tidy data set with the ##
+#    average of each variable for each activity and each subject.                      ##
+#########################################################################################
+
